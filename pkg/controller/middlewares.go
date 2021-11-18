87951f70dbda7bba6f36e81aef7d12bce8580e8e
AzizRahimov <aziz.rahimov0001@gmail.com>
2021-11-18 17:35:03 +0500

    cors

--- pkg/controller/middlewares.go
+++ pkg/controller/middlewares.go
@@ -4,7 +4,7 @@ import "github.com/gin-gonic/gin"
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Content-Type", "application/json")
+		//c.Writer.Header().Set("Content-Type", "application/json")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
