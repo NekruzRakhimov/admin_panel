01d7573fc82e30140114a4d61fa5a3fa9d328106
Aziz Rahimov <aziz.rahimov0001@gmail.com>
2021-11-23 17:44:25 +0500

    убрал комент с бд

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"admin_panel/db"
 	"admin_panel/routes"
 	"admin_panel/utils"
 )
@@ -23,7 +24,7 @@ import (
 func main() {
 	utils.ReadSettings()
 
-//	db.StartDbConnection()
+/db.StartDbConnection()
 
 	routes.RunAllRoutes()
 }
