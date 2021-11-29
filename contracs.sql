24c189d7f990267025d7da5f935f33fb78d18202
AzizRahimov <aziz.rahimov0001@gmail.com>
2021-11-29 21:35:38 +0500

    added contracts schma

---
+++ contracs.sql
@@ -0,0 +1,77 @@
+create table marketing_services_contract
+(
+    id      serial primary key,
+    manager text not null,
+    kam     text not null,
+    status  bool,
+    requisites_id references requisites (id),
+    supplier_company_manager_id references supplier_company_manager (id),
+    contract_parameters_id references contract_parameters (id),
+
+    discountPercent_id references discountPercent (id),
+    created_at timestamp not null DEFAULT current_timestamp
+
+
+);
+
+create table requisites
+(
+    id                  serial primary key,
+    beneficiary         varchar not null,
+    bank_of_beneficiary varchar not null,
+    bin                 varchar not null,
+    iic                 varchar not null,
+    phone               varchar not null,
+    account_number      varchar not null
+);
+
+
+create table if not exists supplier_company_manager
+(
+    id         serial primary key,
+    work_phone varchar not null,
+    email      varchar not null,
+    skype      varchar not null,
+    phone      varchar not null,
+    position   varchar not null,
+    base       varchar not null
+
+);
+
+create table contract_parameters
+(
+    id                          serial primary key,
+    number_of_contract          varchar not null,
+    amount_contract             integer not null,
+    prepayment                  int     not null,
+    date_of_delivery            timestamptz,
+    frequency_deferred_discount varchar not null,
+    delivery_address            varchar not null
+);
+
+create table currency
+(
+    id         serial primary key,
+    name       varchar   not null,
+    created_at timestamp not null DEFAULT current_timestamp
+);
+
+create table products
+(
+    id             serial primary key,
+    product_number varchar not null,
+    price          int     not null,
+    currency_id    int references currency (id)
+);
+
+create table if not exists discountPercent
+(
+    id         serial primary key,
+    type       varchar not null,
+    name       varchar not null,
+    amount     integer not null,
+    is_active  bool default false,
+    grace_days integer not null
+
+
+)
