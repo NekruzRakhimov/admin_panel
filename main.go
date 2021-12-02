84f731ae3c45e917ebc0c2e846858e7e1a3538ba
Aziz Rahimov <aziz.rahimov0001@gmail.com>
2021-12-02 14:13:57 +0500

    change port

--- main.go
+++ main.go
@@ -17,10 +17,12 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:3000
+// @host https://ais-admin-panel.herokuapp.com:8080
 // @BasePath /
 // @schemes http
 func main() {
+
+	//host localhost:3000
 	utils.ReadSettings()
 
 	//db.StartDbConnection()
