d9ef39a0a70a4acc0ba54a6fc98d76217580756a
Aziz Rahimov <aziz.rahimov0001@gmail.com>
2021-12-28 13:26:40 +0500

    on port

--- routes/routes.go
+++ routes/routes.go
@@ -3,11 +3,13 @@ package routes
 import (
 	"admin_panel/pkg/controller"
 	"admin_panel/utils"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"log"
 	"net/http"
+	"os"
+
 	//_ "github.com/rizalgowandy/go-swag-sample/docs/ginsimple" // you need to update github.com/rizalgowandy/go-swag-sample with your own project path
 	_ "admin_panel/docs"
 )
@@ -110,11 +112,11 @@ func runAllRoutes(r *gin.Engine) {
 
 	//	Start server
 
-	//_ = r.Run(fmt.Sprintf("%s:%s", "0.0.0.0", os.Getenv("PORT")))
+	_ = r.Run(fmt.Sprintf("%s:%s", "0.0.0.0", os.Getenv("PORT")))
 	//_ = r.Run(fmt.Sprintf("%s:%s", "localhost", "3001"))
-	if err := r.Run(":3000"); err != nil {
-		log.Fatal(err)
-	}
+	//if err := r.Run(":3000"); err != nil {
+	//	log.Fatal(err)
+	//}
 
 
 }
