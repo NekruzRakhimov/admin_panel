b64b98ff7f9152c22537e763d17ecce3c8c29800
Aziz Rahimov <aziz.rahimov0001@gmail.com>
2022-01-17 15:51:36 +0500

    added notification

--- pkg/service/notifitaion.go
+++ pkg/service/notifitaion.go
@@ -3,6 +3,8 @@ package service
 import (
 	"admin_panel/db"
 	"admin_panel/model"
+	"fmt"
+	"time"
 )
 
 //TODO:
@@ -14,33 +16,30 @@ import (
 
 func Notification() {
 
+	endDateContract := time.Now().Add((24 * 60) * time.Hour)
 
-	//	fmt.Println("ВЫЗОВ")
-	//	//var contracts []model.Contract
-	//	var data string
-	//	//db.GetDBConn().Model(&contracts).Find()
-	//	//	db.GetDBConn().Model(&contracts).Find("contracts")
-	//	//var bDate []byte
-	/////	db.GetDBConn().Raw("SELECT contract_parameters -> 'contract_date' AS data FROM contracts").Scan(&data)
-	//	//for _, value := range contracts {
-	//	//	//TODO: тут будет сравнение
-	//	//
-	//	//}
-	//	//fmt.Println(string(bDate), "бинарные данные")
-	//
-	//	//json.Unmarshal(bDate, &date)
-	//	fmt.Println(data, "ДАТА")
-	var notification []model.Notification
+	var notifications []model.Notification
+	var notification model.Notification
+
 	//db.GetDBConn().Raw("SELECT cars_info -> 'brand' AS brand  FROM cars").Scan(&cars)
-	scan := db.GetDBConn().Raw("SELECT requisites -> 'bin' AS bin, contract_parameters -> 'contract_date' AS end_date, contract_parameters -> contract_number  AS   contract_number, type, supplier_company_manager -> email  AS email FROM contacts").Scan(&notification)
-	if scan.RecordNotFound() == false{
-		// добавить в бд
+	scan := db.GetDBConn().Raw("SELECT requisites -> 'bin' AS bin, contract_parameters -> 'contract_date' AS end_date, contract_parameters -> contract_number  AS   contract_number, type, supplier_company_manager -> email  AS email FROM contacts").Scan(&notifications)
+	for _, value := range notifications {
+		layout := "2006-01-02T15:04:05.000Z"
+		//str := "2014-11-12T11:45:26.371Z"
+		t, err := time.Parse(layout, value.ContractDate)
+		if err != nil {
+			fmt.Println(err)
+		}
+		if endDateContract.After(t) {
+			if scan.RecordNotFound() == false {
+				// если запиши нет, то в этом случае добавлеяем данные в бд
+				db.GetDBConn().Raw("INSERT into notifications (bin, contract_date, contract_number, type, email) VALUES ($1, $2, $3, $4, $5)",
+					value.Bin, value.ContractDate, value.ContractNumber, value.Type, value.Email).Scan(&notification)
+			}
+			//TODO: но если все таки запись найдена, то можем обновить или ничего не делать
+		}
 	}
-	// если запись найдена то обновляем
-	//timeUpContracts := time.Now().Add(1440 * time.Hour)
 
 
-	//TODO:  запихнуть эти даннные в другую таблицу
-	//fmt.Println(cars)
 
 }
