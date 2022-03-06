2ace9a43e1a263ff57b08caf347ac2b78963462b
Aziz <aziz.rahimov0001@gmail.com>
2022-03-06 15:10:14 +0500

    fixed

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"admin_panel/db"
 	"admin_panel/pkg/jobs"
 	"admin_panel/routes"
 	"admin_panel/utils"
@@ -22,10 +23,11 @@ import (
 // @BasePath /
 // @schemes http
 func main() {
+	
 
 	utils.ReadSettings()
 
-//	db.StartDbConnection()
+	db.StartDbConnection()
 	go jobs.RunJobs()
 
 	routes.RunAllRoutes()
