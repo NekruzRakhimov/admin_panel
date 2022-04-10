9ab70efc3587c9082f68879868dd66e302247674
nekruzrakhimov <nekruzusa111@gmail.com>
2022-04-10 17:14:13 +0500

    refactored downloading file route

--- pkg/controller/file.go
+++ pkg/controller/file.go
@@ -31,5 +31,22 @@ func UploadFile(c *gin.Context) {
 func DownloadFile(c *gin.Context) {
 	filePath := c.Query("file_path")
 
+	//c.Writer.Header().Set("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+
+	if strings.HasSuffix(filePath, "pdf") {
+		c.Writer.Header().Set("Content-Type", "application/pdf")
+	} else if strings.HasSuffix(filePath, "docx") {
+		c.Writer.Header().Set("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+	} else if strings.HasSuffix(filePath, "xlsx") {
+		c.Writer.Header().Set("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+	} else if strings.HasSuffix(filePath, "xls") {
+		c.Writer.Header().Set("Content-Type", "application/vnd.ms-excel")
+	} else if strings.HasSuffix(filePath, "txt") {
+		c.Writer.Header().Set("Content-Type", "text/plain")
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"reason": "file extension not found"})
+		return
+	}
+
 	c.File(filePath)
 }
