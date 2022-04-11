ad82b0ace94a563ec53e956b5fe03eccc713bcfb
nekruzrakhimov <nekruzusa111@gmail.com>
2022-04-11 15:11:18 +0500

    refactored downloading file route

--- pkg/controller/file.go
+++ pkg/controller/file.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -35,24 +36,37 @@ func DownloadFile(c *gin.Context) {
 	fmt.Println("<filepath>: ", filePath)
 	//c.Writer.Header().Set("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
 
-	//if strings.HasSuffix(filePath, "pdf") {
-	//	c.Writer.Header().Set("Content-Type", "application/pdf")
-	//} else if strings.HasSuffix(filePath, "docx") {
-	//	c.Writer.Header().Set("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-	//} else if strings.HasSuffix(filePath, "xlsx") {
-	//	c.Writer.Header().Set("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-	//} else if strings.HasSuffix(filePath, "xls") {
-	//	c.Writer.Header().Set("Content-Type", "application/vnd.ms-excel")
-	//} else if strings.HasSuffix(filePath, "txt") {
-	//	c.Writer.Header().Set("Content-Type", "text/plain")
-	//} else {
-	//	c.JSON(http.StatusBadRequest, gin.H{"reason": "file extension not found"})
-	//	return
-	//}
+	contentType, err := GetFileContentType(filePath)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"reason": err.Error()})
+		return
+	}
 
-	c.Writer.Header().Set("Content-Type", "image/png")
+	c.Writer.Header().Set("Content-Type", contentType)
 	c.File(filePath)
 	//c.File("files/contracts/edit-document.docx")
+}
+
+func GetFileContentType(filePath string) (contentType string, err error) {
+	if strings.HasSuffix(filePath, "pdf") {
+		return "application/pdf", nil
+	} else if strings.HasSuffix(filePath, "docx") {
+		return "application/vnd.openxmlformats-officedocument.wordprocessingml.document", nil
+	} else if strings.HasSuffix(filePath, "xlsx") {
+		return "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", nil
+	} else if strings.HasSuffix(filePath, "xls") {
+		return "application/vnd.ms-excel", nil
+	} else if strings.HasSuffix(filePath, "txt") {
+		return "text/plain", nil
+	} else if strings.HasSuffix(filePath, "png") {
+		return "image/png", nil
+	} else if strings.HasSuffix(filePath, "jpeg") {
+		return "image/jpeg", nil
+	} else if strings.HasSuffix(filePath, "jpg") {
+		return "image/jpg", nil
+	} else {
+		return "", errors.New("file extension not found")
+	}
 }
 
 //reportRB_brand1.xlsx
