4ff7234538e78ded1d3ee90016ad27050402750e
Aziz Rahimov <aziz.rahimov0001@gmail.com>
2022-05-13 14:21:21 +0500

    update

--- pkg/repository/contract.go
+++ pkg/repository/contract.go
@@ -380,7 +380,7 @@ func GetSuppliers() (suppliers []models.DataClient, err error) {
 }
 
 func GetSuppliersByParameter(filed string, value string)  (suppliers []models.DataClient, err error) {
-	sqlQuery := fmt.Sprintf("SELECT * FROM suppliers WHERE  %s  LIKE $1", filed)
+	sqlQuery := fmt.Sprintf("SELECT * FROM suppliers WHERE  %s  LIKE ?", filed)
 	//sqlQuery := "SELECT * FROM suppliers WHERE  client_name $1"
 	err  =db.GetDBConn().Raw(sqlQuery, "%"+value+"%").Scan(&suppliers).Error
 	if err != nil {
