99f6f1e823b8ff115951a9ee5fdd3efd8dea7e1f
nekruzrakhimov <nekruzusa111@gmail.com>
2022-05-15 14:19:22 +0500

    reformated_date in graphics

--- pkg/repository/auto_orders.go
+++ pkg/repository/auto_orders.go
@@ -14,7 +14,24 @@ func CreateGraphic(graphic models.Graphic) error {
 }
 
 func GetAllGraphics() (graphics []models.Graphic, err error) {
-	sqlQuery := "SELECT * FROM graphics"
+	sqlQuery := `SELECT id,
+					   number,
+					   author,
+					   supplier_name,
+					   supplier_code,
+					   region_name,
+					   region_code,
+					   store_name,
+					   store_code,
+					   nomenclature_group,
+					   execution_period,
+					   once_a_month,
+					   twice_a_month,
+					   is_on,
+					   to_char(auto_order_date::date, 'DD.MM.YYYY'),
+					   created_at,
+					   application_day
+				from graphics`
 	if err = db.GetDBConn().Raw(sqlQuery).Scan(&graphics).Error; err != nil {
 		return nil, err
 	}
@@ -23,7 +40,24 @@ func GetAllGraphics() (graphics []models.Graphic, err error) {
 }
 
 func GetGraphicByID(id int) (graphic models.Graphic, err error) {
-	sqlQuery := "SELECT * FROM graphics WHERE id = ?"
+	sqlQuery := `SELECT id,
+					   number,
+					   author,
+					   supplier_name,
+					   supplier_code,
+					   region_name,
+					   region_code,
+					   store_name,
+					   store_code,
+					   nomenclature_group,
+					   execution_period,
+					   once_a_month,
+					   twice_a_month,
+					   is_on,
+					   to_char(auto_order_date::date, 'DD.MM.YYYY'),
+					   created_at,
+					   application_day
+				from graphics WHERE id = ?`
 	if err = db.GetDBConn().Raw(sqlQuery, id).Scan(&graphic).Error; err != nil {
 		return models.Graphic{}, err
 	}
