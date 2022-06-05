40f82cd223205ad29dae31cee14d7909ecb98b90
Aziz <aziz.rahimov0001@gmail.com>
2022-06-06 00:50:45 +0500

    fixed search

--- sql/brands.sql
+++ sql/brands.sql
@@ -1,15 +1,18 @@
-
-create table brands (
-                        id bigserial primary key,
-                      --  contract_number  text not null,
-                        brand text not null,
-                        brand_code text not null ,
-                        discount_percent varchar not null,
-                        contract_id integer references contracts(id)
+create table brands
+(
+    id               bigserial primary key,
+    --  contract_number  text not null,
+    brand            text    not null,
+    brand_code       text    not null,
+    discount_percent varchar not null,
+    contract_id      integer references contracts (id)
 );
 
 
-SELECT id, bin, contract_parameters ->> 'contract_number' AS contract_number FROM contracts WHERE status = 'в работе' AND requisites ->> 'bin' = '160140011654';
+SELECT id, bin, contract_parameters ->> 'contract_number' AS contract_number
+FROM contracts
+WHERE status = 'в работе'
+  AND requisites ->> 'bin' = '160140011654';
 
 create table brands
 (
@@ -22,36 +25,55 @@ create table brands
 
 drop table brands;
 
-SELECT c.id, b.contract_id, c.contract_parameters ->> 'contract_number' AS contract_number, b.discount_percent, b.brand
+SELECT c.id,
+       b.contract_id,
+       c.contract_parameters ->> 'contract_number' AS contract_number,
+       b.discount_percent,
+       b.brand
 FROM brands b
-JOIN contracts  c ON b.contract_id = c.id
-WHERE contract_id  = '390';
+         JOIN contracts c ON b.contract_id = c.id
+WHERE contract_id = '390';
 --WHERE requisites ->> 'bin' = '080240011774';
 
 
+SELECT id
+FROM contracts
+WHERE requisites ->> 'bin' = '960340000029';
+
+SELECT discounts
+FROM contracts;
+
+SELECT brand, discount_percent
+FROM brands
+WHERE contract_id = 207;
 
 
-SELECT id FROM contracts WHERE requisites ->> 'bin' = '960340000029';
 
-SELECT  discounts FROM contracts;
-
-SELECT brand, discount_percent FROM  brands WHERE contract_id = 207;
-
-
-
-SELECT c.id, b.contract_id, c.contract_parameters ->> 'contract_number' AS contract_number, b.discount_percent, b.brand FROM contracts c
-		JOIN brands  b ON b.contract_id = c.id WHERE c.requisites ->> 'bin' = '160140011654';
+SELECT c.id,
+       b.contract_id,
+       c.contract_parameters ->> 'contract_number' AS contract_number,
+       b.discount_percent,
+       b.brand
+FROM contracts c
+         JOIN brands b ON b.contract_id = c.id
+WHERE c.requisites ->> 'bin' = '160140011654';
 
 --SELECT id, brand, discount_percent FROM  brands =  where   contract_id
 
-DELETE FROM contracts WHERE  id =248;
+DELETE
+FROM contracts
+WHERE id = 248;
 
 
-SELECT  * FROM contracts WHERE id = 294;
+SELECT *
+FROM contracts
+WHERE id = 294;
 
 
 
-SELECT * from contracts WHERE req
+SELECT *
+from contracts
+WHERE req
 
 
 
@@ -66,81 +88,131 @@ SELECT * from contracts WHERE req
 ---Итог
 
 
+INSERT INTO brands(brand, discount_percent, contract_id)
+VALUES ($1, $2, $3);
 
 
-
-
-INSERT INTO brands(brand, discount_percent, contract_id) VALUES ($1, $2, $3);
-
-
-SELECT id, contract_parameters ->> 'contract_number' AS contract_number, discounts ->> 'discount_amount' AS discount_amount, requisites ->> 'bin' AS bin FROM contracts
+SELECT id,
+       contract_parameters ->> 'contract_number' AS contract_number,
+       discounts ->> 'discount_amount'           AS discount_amount,
+       requisites ->> 'bin'                      AS bin
+FROM contracts
 WHERE requisites ->> 'bin' = '860418401075';
 
 
-SELECT discounts ->> 'discount_amount' AS discount_amount FROM contracts where  id = 260;
+SELECT discounts ->> 'discount_amount' AS discount_amount
+FROM contracts
+where id = 260;
 
 
-SELECT discounts FROM contracts WHERE id = 260;
+SELECT discounts
+FROM contracts
+WHERE id = 260;
 
-SELECT id, discounts FROM contracts
+SELECT id, discounts
+FROM contracts
 WHERE requisites ->> 'bin' = '070340005201';
 
-SELECT arr.position,arr.item_object
+SELECT arr.position, arr.item_object
 FROM purchases,
      jsonb_array_elements(items_purchased) with ordinality arr(item_object, position)
-WHERE id=2;
+WHERE id = 2;
 
 
-SELECT discounts  FROM contracts WHERE requisites ->> 'bin' = '090909098988';
-SELECT id, discounts::json as discount  FROM contracts WHERE requisites ->> 'bin' = '070340005201';
-SELECT json_array_elements(discounts ->'periods') FROM  contracts;
+SELECT discounts
+FROM contracts
+WHERE requisites ->> 'bin' = '090909098988';
+SELECT id, discounts::json as discount
+FROM contracts
+WHERE requisites ->> 'bin' = '070340005201';
+SELECT json_array_elements(discounts -> 'periods')
+FROM contracts;
 
 WHERE requisites ->> 'bin' = '070340005201';
 
 
-SELECT discounts ->> 'periods'  an  FROM contracts WHERE requisites ->> 'bin' = '070340005201';
+SELECT discounts ->> 'periods' an
+FROM contracts
+WHERE requisites ->> 'bin' = '070340005201';
 
 
 
-SELECT id, manager, ext_contract_code FROM contracts WHERE requisites ->> 'bin' =  '100840008133';
+SELECT id, manager, ext_contract_code
+FROM contracts
+WHERE requisites ->> 'bin' = '100840008133';
 
 
-SELECT  id, requisites ->> 'bin' as BIN, contract_parameters ->> 'start_date' as start,  contract_parameters ->> 'end_date' as end_date from contracts
+SELECT id,
+       requisites ->> 'bin'                 as BIN,
+       contract_parameters ->> 'start_date' as start,
+       contract_parameters ->> 'end_date'   as end_date
+from contracts
 
 
-SELECT  id, requisites ->> 'bin' as BIN, contract_parameters ->> 'start_date' as start,  contract_parameters ->> 'end_date' as end_date from contracts
+SELECT id,
+       requisites ->> 'bin'                 as BIN,
+       contract_parameters ->> 'start_date' as start,
+       contract_parameters ->> 'end_date'   as end_date
+from contracts
 WHERE requisites ->> 'bin' = '860418401075';
 
-Select contract_parameters ->> 'end_date' as end_date FROM contracts;
+Select contract_parameters ->> 'end_date' as end_date
+FROM contracts;
 
 
-insert into brands (brand, discount_percent, contract_id) VALUES ('Старт', 5, 285);
+insert into brands (brand, discount_percent, contract_id)
+VALUES ('Старт', 5, 285);
 
 
-SELECT *From brands ;
-
-
-
-Update contracts SET ext_contract_code = 'K0054437' WHERE requisites ->> 'bin' =  '100840008133';
-
-Update contracts SET status = 'заверщённый' where id = ?
-
-
-SELECT * FROM brands WHERE  contract_id = 285;
-
-SELECT *FROM  contracts where  id = 285;
+SELECT *
+From brands;
 
 
 
+Update contracts
+SET ext_contract_code = 'K0054437'
+WHERE requisites ->> 'bin' = '100840008133';
 
-DELETE FROM contracts WHERE  id = 264;
+Update contracts
+SET status = 'заверщённый'
+where id = ?
+
+
+SELECT *
+FROM brands
+WHERE contract_id = 285;
+
+SELECT *
+FROM contracts
+where id = 285;
+
+
+
+DELETE
+FROM contracts
+WHERE id = 264;
 
 ALTER TABLE dictionary_values
     ADD COLUMN has_file boolean default false;
 
 ALTER TABLE dictionary_values
-    DROP COLUMN  has_file ;
+    DROP COLUMN has_file;
 
 
 
-SELECT  *FROM suppliers WHERE client_name ILIKE  '%рад%';
+SELECT *
+FROM suppliers
+WHERE client_name ILIKE '%рад%';
+
+
+
+SELECT id
+     , status
+     , requisites ->> 'beneficiary'              AS beneficiary
+     , contract_parameters ->> 'contract_number' AS contract_number,type AS contract_type
+     , created_at
+     , updated_at
+     , manager                                   AS author
+     , contract_parameters ->> 'contract_amount' AS amount
+FROM contracts
+WHERE contract_number like  '%9595_2%' AND status =  'на согласовании'
