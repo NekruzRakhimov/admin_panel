ccfb7fac2e7bb317003d4481d8ccc00e0be22f52
nekruzrakhimov <nekruzusa111@gmail.com>
2022-06-20 15:48:32 +0500

    refactored sending segments

--- pkg/controller/segment.go
+++ pkg/controller/segment.go
@@ -70,61 +70,70 @@ func GetSegmentByID(c *gin.Context) {
 }
 
 func SendLetter(c *gin.Context) {
-	formedGraphicID, err := strconv.Atoi(c.Param("id"))
-	fmt.Println(formedGraphicID, "ID")
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"reason": "ERROR"})
-		return
-	}
+	//formedGraphicID, err := strconv.Atoi(c.Param("id"))
+	//fmt.Println(formedGraphicID, "ID")
+	//if err != nil {
+	//	c.JSON(http.StatusBadRequest, gin.H{"reason": "ERROR"})
+	//	return
+	//}
 
-	//сформированый потребность граффика -
-	formedGraphic, err := service.GetFormedGraphicByID(formedGraphicID)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
-		return
-	}
-
-	fmt.Println("DATA", formedGraphic)
-
-	formedGraphicProducts, err := service.GetAllFormedGraphicsProducts(formedGraphicID)
+	formedGraphics, err := service.GetAllFormedGraphics()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
 		return
 	}
 
-	graphic, err := service.GetGraphicByID(formedGraphic.GraphicID)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
-		return
-	}
-	fmt.Println("GRAP", graphic)
-	fmt.Println("GRAP", graphic)
-
-	c.JSON(200, gin.H{
-		"formedGraphic":         formedGraphic,
-		"formedGraphicProducts": formedGraphicProducts,
-		"graphic":               graphic,
-	})
-
-	if formedGraphic.Status == "сформирован" {
-		err := service.ChangeLetter(formedGraphic.ID)
+	for _, graphic := range formedGraphics {
+		//сформированый потребность граффика -
+		formedGraphic, err := service.GetFormedGraphicByID(graphic.ID)
 		if err != nil {
-			c.JSON(400, gin.H{"reason": err})
+			c.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
 			return
 		}
 
-		service.FillSegment(formedGraphic, formedGraphicProducts, graphic)
-		segment, _ := service.GetSegment(graphic.SupplierName)
-		var email string
-		if segment.Email != "" {
-			email = segment.Email
-			service.SendNotificationSegment("files/segments/segment.xlsx", email)
-		} else {
-			for _, value := range segment.Region {
-				email = value.Email
-				service.SendNotificationSegment("files/segments/segment.xlsx", email)
-			}
+		fmt.Println("DATA", formedGraphic)
+
+		formedGraphicProducts, err := service.GetAllFormedGraphicsProducts(graphic.ID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
 		}
-		//service.SendNotificationSegment("files/segments/segment.xlsx")
+
+		graphic, err := service.GetGraphicByID(formedGraphic.GraphicID)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"reason": err.Error()})
+			return
+		}
+		fmt.Println("GRAP", graphic)
+		fmt.Println("GRAP", graphic)
+
+		c.JSON(200, gin.H{
+			"formedGraphic":         formedGraphic,
+			"formedGraphicProducts": formedGraphicProducts,
+			"graphic":               graphic,
+		})
+
+		if formedGraphic.Status == "сформирован" {
+			err := service.ChangeLetter(formedGraphic.ID)
+			if err != nil {
+				c.JSON(400, gin.H{"reason": err})
+				return
+			}
+
+			service.FillSegment(formedGraphic, formedGraphicProducts, graphic)
+			segment, _ := service.GetSegment(graphic.SupplierName)
+			var email string
+			if segment.Email != "" {
+				email = segment.Email
+				service.SendNotificationSegment("files/segments/segment.xlsx", email)
+			} else {
+				for _, value := range segment.Region {
+					email = value.Email
+					service.SendNotificationSegment("files/segments/segment.xlsx", email)
+				}
+			}
+			//service.SendNotificationSegment("files/segments/segment.xlsx")
+		}
 	}
+
 }
